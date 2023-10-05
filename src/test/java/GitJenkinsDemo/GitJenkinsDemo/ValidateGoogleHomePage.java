c5101c7b95098b2f3295b211ed294b8dd5584779
veena <shravanthi.gvs@gmail.com>
2023-10-05 11:29:54 -0500

    first commit

---
+++ src/test/java/GitJenkinsDemo/GitJenkinsDemo/ValidateGoogleHomePage.java
@@ -0,0 +1,47 @@
+package GitJenkinsDemo.GitJenkinsDemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ValidateGoogleHomePage {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void setup() {
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(option);
+
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}	
+		@Test
+		public void validateHomePage()
+		{
+			driver.findElement(By.id("APjFqb")).sendKeys("Numpy Ninja Hackathon");
+			driver.findElement(By.name("btnK")).click();
+			driver.getTitle();
+		}
+		
+		@AfterTest
+		public void teardown()
+		  {
+			driver.close();
+		  }
+		
+		
+	}
+
